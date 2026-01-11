214c5da272bd233b160dcd297b8865cfee5bf652
Ayan Khan <beaayank@gmail.com>
2026-01-11 20:39:22 +0530

    implement coin overview and trending coins ui and functionality using the fetcher utility function

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import Image from 'next/image';
+import DataTable from '@/components/DataTable';
+import Link from 'next/link';
+import { cn, formatCurrency } from '@/lib/utils';
+import { TrendingDown, TrendingUp } from 'lucide-react';
+import { fetcher } from '@/lib/coingecko.action';
+import CoinOverview from '@/components/home/CoinOverview';
+import TrendingCoins from '@/components/home/TrendingCoins';
+import { CoinOverviewFallback, TrendingCoinsFallback } from '../components/home/fallback';
 
-const page = () => {
+
+
+
+
+
+const page = async () => {
+
+
   return (
-    <div></div>
+    <main className="main-container">
+      <section className='home-grid'>
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+
+    </main>
   )
 }
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
     <header>
       <div className="main-container inner">
         <Link href="/">
-          <Image src="/coinwave logo.svg" alt="CoinWave logo" width={132} height={40} />
+          <Image src="/coinwave logo.svg" alt="CoinWave logo" width={160} height={40} />
         </Link>
 
         <nav>
